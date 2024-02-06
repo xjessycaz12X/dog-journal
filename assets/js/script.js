204735b336d89ea457431a506e6f0a9de49f9ece
Joseph Kamil <kamilj@umich.edu>
2024-02-05 22:41:23 -0800

    added facts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+var dogFactsEL = document.getElementById('dogFacts');
+var dogPicsEl = document.getElementById('dogPics');
+
+
 //code for the api for random dog photo
 
 fetch("https://dog.ceo/api/breeds/image/random")
@@ -17,9 +21,33 @@ fetch("https://dog.ceo/api/breeds/image/random")
   });
 function displayImages(images) {
   images.forEach((image) => {
-    const imgElement = document.createElement("img");
+    var imgElement = document.createElement("img");
     imgElement.src = image;
     imgElement.alt = "Random Dog Image";
-    document.body.appendChild(imgElement);
+    dogPicsEl.appendChild(imgElement);
   });
 }
+
+
+fetch("https://dogapi.dog/api/v2/facts?limit=10")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    console.log(response);
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    displayFacts(data);
+  })
+  .catch((err) => {
+    console.error("Fetch error:", err);
+  });
+function displayFacts(facts) {
+  console.log(facts.data);
+  const pElement = document.createElement("p");
+  pElement.textContent = facts.data[0].attributes.body;
+  dogFactsEL.appendChild(pElement);
+}
+
