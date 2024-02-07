40b4174387d76eea528c99205c00239462c90ffe
rbaker4559 <reginaldsbaker10@icloud.com>
2024-02-06 20:11:51 -0800

    adding local storage js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ fetch("https://dogapi.dog/api/v2/facts?limit=10")
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
-    
+
     console.log(response);
     return response.json();
   })
@@ -17,8 +17,9 @@ fetch("https://dogapi.dog/api/v2/facts?limit=10")
   .catch((err) => {
     console.error("Fetch error:", err);
   });
+
+//displays dog facts w/in dog facts pane
 function displayFacts(facts) {
-  console.log(facts.data);
   const pElement = document.createElement("p");
   pElement.textContent = facts.data[0].attributes.body;
   dogFactsEL.appendChild(pElement);
@@ -41,6 +42,8 @@ fetch("https://dog.ceo/api/breeds/image/random")
   .catch((err) => {
     console.error("Fetch error:", err);
   });
+
+//display images w/in dog image container on html
 function displayImages(images) {
   images.forEach((image) => {
     const imgElement = document.createElement("img");
@@ -63,48 +66,66 @@ let journalPane = document.querySelector(".journal-pane");
 
 //local storage variables
 let currentIndex = 0;
-let journalLog = {};
+let journalLog = JSON.parse(localStorage.getItem("dogJournal")) || [];
+
 let dogJournal = {
   entries: journalLog,
-  index: currentIndex,
 };
 
 //eventListeners
-save.addEventListener("click", function(event){
+save.addEventListener("click", function (event) {
   createEntry(event);
 });
 
-
-
 //functions
-function createEntry(event){
+
+//renders journal entries on page via localstorage
+function renderJournalEntries() {
+  journalLog = JSON.parse(localStorage.getItem("dogJournal"));
+  journalPane.innerHTML = "";
+
+  for (let i = 0; i < journalLog.length; i++) {
   
+    const newLI = document.createElement("li");
+    newLI.classList = "mt-3";
+
+    const newSpan = document.createElement("span");
+    newSpan.textContent = journalLog[i];
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList = "btn btn-primary btn-sm me-5";
+    deleteButton.textContent = "Delete";
+    deleteButton.type = "button delete";
+    deleteButton.addEventListener("click", function (event) {
+      deleteEntry(event);
+    })
+
+    newLI.appendChild(deleteButton);
+    newLI.appendChild(newSpan);
+    journalPane.appendChild(newLI);
+  }
+}
+
+//refreshes page and appends DOM
+function createEntry() {
+
   //creates DOM journal entry
   let entry = textArea.value;
 
-  const newLI = document.createElement("li");
-  newLI.classList = "mt-3";
-
-  const newSpan = document.createElement("span");
-  newSpan.textContent = entry;
-
-  const deleteButton = document.createElement("button");
-  deleteButton.classList = "btn btn-primary btn-sm me-5";
-  deleteButton.textContent = "Delete";
-  deleteButton.type = "button delete";
-  deleteButton.addEventListener("click", function(event) {
-    deleteEntry(event);
-  })
-
-  newLI.appendChild(deleteButton);
-  newLI.appendChild(newSpan);
-  journalPane.appendChild(newLI);
-
   //resets 
   textArea.value = "";
+
+  //add to local storage
+  journalLog.push(entry);
+  localStorage.setItem("dogJournal", JSON.stringify(journalLog));
+  renderJournalEntries();
 }
 
-function deleteEntry(event){
-  event.target.parentNode.remove(event);
-  currentIndex--;
+//filters array and local storage then refreshes page
+function deleteEntry(event) {
+  const updatedJournalLog = journalLog.filter(item => item !== event.target.nextElementSibling.textContent);
+  localStorage.setItem("dogJournal", JSON.stringify(updatedJournalLog));
+  renderJournalEntries();
 }
+
+renderJournalEntries();
